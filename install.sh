b74ffd9c715948a2fb381f1578a5cdad47a825f2
RafaelCz <RafaelCz@users.noreply.github.com>
2017-09-23 21:58:40 +0200

    modified install script, needs further testing.

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-for i in $(ls -d */); do
-	stow ${i%%/}
+set -o errexit
+set -o pipefail
+set -o nounset
+
+for D in *; do
+	if [ -d "${D}" ]; then
+		echo "${D}: " && stow "${D}" 2> /dev/null && echo "linked" || echo "failed"
+	fi
 done
